96bf2ad54eae4ba839a640bec03f3ef4920e4d6a
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-05-17 10:48:19 +0300

    Moidfy console conf

--- protected/config/console.php
+++ protected/config/console.php
@@ -4,7 +4,7 @@
 // Any writable CConsoleApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'Futural Bank',
+	'name'=>'Futural bank',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
@@ -28,28 +28,4 @@ return array(
 			),
 		),
 	),
-        'modules'=>array(
-                'user'=>array(
-                    # encrypting method (php hash function)
-                    'hash' => 'md5',
-                    # send activation email
-                    'sendActivationMail' => true,
-                    # allow access for non-activated users
-                    'loginNotActiv' => false,
-                    # activate user on registration (only sendActivationMail = false)
-                    'activeAfterRegister' => false,
-                    # automatically login from registration
-                    'autoLogin' => true,
-                    # registration path
-                    'registrationUrl' => array('/user/registration'),
-                    # recovery password path
-                    'recoveryUrl' => array('/user/recovery'),
-                    # login form path
-                    'loginUrl' => array('/user/login'),
-                    # page after login
-                    'returnUrl' => array('/user/profile'),
-                    # page after logout
-                    'returnLogoutUrl' => array('/user/login'),
-                ),
-        ),
 );

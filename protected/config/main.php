f1900c00040fa428b0b03e5715dfa3fbbc7b634f
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-05-17 10:54:26 +0300

    Modify user conf Modify rights conf

--- protected/config/main.php
+++ protected/config/main.php
@@ -28,13 +28,47 @@ return array(
                     'password'=>'futural',
                     'ipFilters'=>array('127.0.0.1','::1'),
             ),
-            'user'=>array(
-                    'tableUsers' => 'users',
-                    'tableProfiles' => 'profiles',
-                    'tableProfileFields' => 'profiles_fields',
-            ),
+        'user'=>array(
+                'tableUsers' => 'user',
+                'tableProfiles' => 'profile',
+                'tableProfileFields' => 'profile_fields',
+                # encrypting method (php hash function)
+                'hash' => 'md5',
+                # send activation email
+                'sendActivationMail' => true,
+                # allow access for non-activated users
+                'loginNotActiv' => false,
+                # activate user on registration (only sendActivationMail = false)
+                'activeAfterRegister' => false,
+                # automatically login from registration
+                'autoLogin' => true,
+                # registration path
+                'registrationUrl' => array('/user/registration'),
+                # recovery password path
+                'recoveryUrl' => array('/user/recovery'),
+                # login form path
+                'loginUrl' => array('/user/login'),
+                # page after login
+                'returnUrl' => array('/user/profile'),
+                # page after logout
+                'returnLogoutUrl' => array('/user/login'),
+           ),
             'rights'=>array(
-                    'install'=>true,
+                'superuserName'=>'Admin', // Name of the role with super user privileges. 
+                'authenticatedName'=>'Authenticated',  // Name of the authenticated user role. 
+                'userIdColumn'=>'id', // Name of the user id column in the database. 
+                'userNameColumn'=>'username',  // Name of the user name column in the database. 
+                'enableBizRule'=>true,  // Whether to enable authorization item business rules. 
+                'enableBizRuleData'=>true,   // Whether to enable data for business rules. 
+                'displayDescription'=>true,  // Whether to use item description instead of name. 
+                'flashSuccessKey'=>'RightsSuccess', // Key to use for setting success flash messages. 
+                'flashErrorKey'=>'RightsError', // Key to use for setting error flash messages. 
+                'baseUrl'=>'/rights', // Base URL for Rights. Change if module is nested. 
+                'layout'=>'rights.views.layouts.main',  // Layout to use for displaying Rights. 
+                'appLayout'=>'application.views.layouts.main', // Application layout. 
+                'cssFile'=>'rights.css', // Style sheet file to use for Rights. 
+                'install'=>false,  // Whether to enable installer. 
+                'debug'=>false, 
             ),
         ),
 

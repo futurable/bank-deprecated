196e66ca8020ecfeb0bfd24d971d5e05e92585d4
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-05-18 10:12:22 +0300

    Modify access control

--- protected/components/Controller.php
+++ protected/components/Controller.php
@@ -20,4 +20,23 @@ class Controller extends CController
 	 * for more details on how to specify this property.
 	 */
 	public $breadcrumbs=array();
+        
+        public function filters()
+        {
+            return array( 'accessControl' ); // perform access control for CRUD operations
+        }
+
+        public function accessRules()
+        {
+            return array(
+                array('allow', // allow authenticated users to access all actions
+                    'users'=>array('@'),
+                ),
+                array('allow',
+                    'actions'=>array('login'),
+                    'users'=>array('*'),
+                ),
+                array('deny'),
+            );
+        }
 }

776fbaa5f7603acfc903d2f3e63783de194fb050
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-05-18 10:30:42 +0300

    Modify return url

--- protected/config/main.php
+++ protected/config/main.php
@@ -49,7 +49,7 @@ return array(
                 # login form path
                 'loginUrl' => array('/user/login'),
                 # page after login
-                'returnUrl' => array('/user/profile'),
+                'returnUrl' => array('/'),
                 # page after logout
                 'returnLogoutUrl' => array('/user/login'),
            ),

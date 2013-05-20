aa5ad7b8bb377d94176b0ceb2250cc21de687433
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-05-20 10:11:22 +0300

    Remove obsolete fields

--- protected/modules/user/views/user/login.php
+++ protected/modules/user/views/user/login.php
@@ -5,7 +5,7 @@ $this->breadcrumbs=array(
 );
 ?>
 
-<h1><?php echo UserModule::t("Login"); ?></h1>
+<h1><?php echo UserModule::t("Login to Futural bank"); ?></h1>
 
 <?php if(Yii::app()->user->hasFlash('loginMessage')): ?>
 
@@ -15,12 +15,8 @@ $this->breadcrumbs=array(
 
 <?php endif; ?>
 
-<p><?php echo UserModule::t("Please fill out the following form with your login credentials:"); ?></p>
-
 <div class="form">
 <?php echo CHtml::beginForm(); ?>
-
-	<p class="note"><?php echo UserModule::t('Fields with <span class="required">*</span> are required.'); ?></p>
 	
 	<?php echo CHtml::errorSummary($model); ?>
 	
@@ -32,18 +28,7 @@ $this->breadcrumbs=array(
 	<div class="row">
 		<?php echo CHtml::activeLabelEx($model,'password'); ?>
 		<?php echo CHtml::activePasswordField($model,'password') ?>
-	</div>
-	
-	<div class="row">
-		<p class="hint">
-		<?php echo CHtml::link(UserModule::t("Register"),Yii::app()->getModule('user')->registrationUrl); ?> | <?php echo CHtml::link(UserModule::t("Lost Password?"),Yii::app()->getModule('user')->recoveryUrl); ?>
-		</p>
-	</div>
-	
-	<div class="row rememberMe">
-		<?php echo CHtml::activeCheckBox($model,'rememberMe'); ?>
-		<?php echo CHtml::activeLabelEx($model,'rememberMe'); ?>
-	</div>
+        </div>
 
 	<div class="row submit">
 		<?php echo CHtml::submitButton(UserModule::t("Login")); ?>

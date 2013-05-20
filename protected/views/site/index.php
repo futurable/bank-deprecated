e3db3eb200d45a19f0374b31329c7163fccf41ad
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-05-20 12:32:40 +0300

    Add placeholder index text

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,7 +1,9 @@
 <?php
 /* @var $this SiteController */
 
-$this->pageTitle=Yii::app()->name;
-?>
+    $this->pageTitle=Yii::app()->name;
 
-<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
+    echo "<h1>";
+    echo Yii::t("FrontPage", "WelcomeText");
+    echo "</h1>";
+?>

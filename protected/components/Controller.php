4b42d0817ba47c8974db2be367261e458c41395c
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-05-20 15:59:48 +0300

    Add global WebUser to controller

--- protected/components/Controller.php
+++ protected/components/Controller.php
@@ -20,9 +20,14 @@ class Controller extends CController
 	 * for more details on how to specify this property.
 	 */
 	public $breadcrumbs=array();
+        public $WebUser;
         
         function init()
         {
+            if(isset(Yii::app()->user->id)){
+                $this->WebUser= User::model()->findbyPk( Yii::app()->user->id );
+            }
+                
             parent::init();
             $app = Yii::app();
             if (isset($_POST['_lang']))

af3219a6ae062f5bd23e35afcfd9c998796aef9e
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-05-21 17:54:13 +0300

    Add partial Finnish translation

---
+++ protected/modules/user/messages/fi/user.php
@@ -0,0 +1,16 @@
+<?php
+/**
+* (c) Vitaliy Stepanenko,
+* (c) Alexander Listunov
+*/
+return array(
+        'Login to Futural bank' => 'Kirjaudu Futural Pankkiin',
+	'Registration' => 'Rekisteröityminen',
+	'Register' => 'Rekisteröidy',
+	'Login' => 'Kirjaudu sisään',
+	'Logout' => 'Kirjaudu ulos',
+	'username' => 'Käyttäjännimi',
+	'password' => 'Salasana',
+        'Username is incorrect.' => 'Käyttäjännimi on virheellinen.',
+        'Password is incorrect.' => 'Salasana on virheellinen.',
+);

1526f74f11dbea0b4a91b869009ae33748af2bf1
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-05-21 09:56:56 +0300

    Add translation

--- protected/views/accountTransaction/create.php
+++ protected/views/accountTransaction/create.php
@@ -1,4 +1,4 @@
-<h1>Create Transaction</h1>
+<h1><?php echo Yii::t('AccountTransaction', 'CreateTransaction'); ?></h1>
 
 <?php echo $this->renderPartial('_formStep1', array('model'=>$model)); ?>
 <?php // echo $this->renderPartial('_form', array('model'=>$model)); ?>

0c75adf3eb2a17dbdc22bce393d121239021f095
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-05-21 11:01:06 +0300

    Add form step logic

--- protected/views/accountTransaction/create.php
+++ protected/views/accountTransaction/create.php
@@ -1,4 +1,7 @@
 <h1><?php echo Yii::t('AccountTransaction', 'CreateTransaction'); ?></h1>
 
-<?php echo $this->renderPartial('_formStep1', array('model'=>$model)); ?>
-<?php // echo $this->renderPartial('_form', array('model'=>$model)); ?>
+<?php
+if($accountTransaction->form_step === 1) echo $this->renderPartial('_formStep1', array('accountTransaction'=>$accountTransaction));
+elseif($accountTransaction->form_step === 2) echo $this->renderPartial('_formStep2', array('accountTransaction'=>$accountTransaction));
+else echo "Error while deciding form step";
+?>

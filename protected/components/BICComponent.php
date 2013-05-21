7f7da9fd986afb6a420f37fb88d074ca7016c926
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-05-21 15:01:30 +0300

    Move components to right place

---
+++ protected/components/BICComponent.php
@@ -0,0 +1,12 @@
+<?php
+class BICComponent extends CFormatter
+{
+    public function getBICFromIBAN($IBAN){
+        return "OIVAFIT0";
+    }
+    
+    public function getBICFromBranchCode($BranchCode) {
+        
+    }
+}
+?>

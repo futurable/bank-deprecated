2800d359189fd17a8f4dc84dd21e283b510f0759
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-05-22 15:53:13 +0300

    Add reference number validator

---
+++ protected/extensions/validators/validReferenceNumber.php
@@ -0,0 +1,40 @@
+<?php
+class validReferenceNumber extends CValidator
+{
+    
+    protected function validateAttribute($object, $attribute){
+        $value=$object->$attribute;
+        
+        // Archive number without verificaton number
+        $cleanNumber = substr($value, 0, -1);
+
+        // Verification numbers
+        $verificationNumber = substr($value, -1);
+        $verificationValue = $this->getReferenceNumberVerificationNumber($cleanNumber);
+
+        if($verificationNumber != $verificationValue){
+            $this->addError($object, $attribute, "Invalid reference number");
+        }
+    }
+    
+    /**
+     * Function for calculating the verification number for reference number
+     * 
+     * @access 	public
+     * @param	number	$number					Number from which the verification number is calculated
+     * @return 	int  	$verificationNumber		Calculated verification number
+     */
+    private function getReferenceNumberVerificationNumber($number) {
+            $number = strval($number);
+            $weight = array(7, 3, 1);
+            $sum = 0;
+
+            for( $i = strlen($number)-1, $j=0; $i>=0; $i--,$j++){
+            $sum += (int) $number[$i] * (int) $weight[$j%3];
+            }
+
+            $verificationNumber = (10-($sum%10))%10;
+            return $verificationNumber;
+    }
+}
+?>

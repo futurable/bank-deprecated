34a6bb6fe5c8d7ce2d960d4e8d180b137ed62d11
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-05-22 14:08:09 +0300

    Add BIC

--- protected/controllers/Account/CreateBankAccountAction.php
+++ protected/controllers/Account/CreateBankAccountAction.php
@@ -7,12 +7,21 @@ class CreateBankAccountAction extends CAction
         $Account=new Account;
 
         // Create predefined values
+        
+        // IBAN
         $branchCode = '970300'; // TODO: get this from conf
         $accountNumber = 1;
         $bban = BBANComponent::generateFinnishBBANaccount($branchCode, $accountNumber);
         $bbanAccountNumber = substr($bban, -6);
         $iban = IBANComponent::generateFinnishIBANaccount($branchCode, $bbanAccountNumber);
         $Account->iban = $iban;
+        
+        // BIC
+        $bic = Bic::model()->find(array('select'=>'id, bic',
+            'condition'=>'branch_code=:branch_code',
+            'params'=>array(':branch_code'=>$branchCode),
+        ))->id;
+        $Account->bank_bic_id = $bic;
         
         if(isset($_POST['Account']))
         {

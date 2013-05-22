4d1e90621227703eda65ad2a63c318187802db4d
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-05-22 13:55:34 +0300

    Add IBAN calculation

--- protected/controllers/Account/CreateBankAccountAction.php
+++ protected/controllers/Account/CreateBankAccountAction.php
@@ -6,6 +6,14 @@ class CreateBankAccountAction extends CAction
         $controller=$this->getController();
         $Account=new Account;
 
+        // Create predefined values
+        $branchCode = '970300'; // TODO: get this from conf
+        $accountNumber = 1;
+        $bban = BBANComponent::generateFinnishBBANaccount($branchCode, $accountNumber);
+        $bbanAccountNumber = substr($bban, -6);
+        $iban = IBANComponent::generateFinnishIBANaccount($branchCode, $bbanAccountNumber);
+        $Account->iban = $iban;
+        
         if(isset($_POST['Account']))
         {
             $Account->attributes=$_POST['Account'];

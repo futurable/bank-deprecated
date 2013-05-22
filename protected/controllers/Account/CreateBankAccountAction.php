c07ab9baa947aab044e19d69ea533dad1cb2a08a
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-05-22 11:57:04 +0300

    Remove saving feature

--- protected/controllers/Account/CreateBankAccountAction.php
+++ protected/controllers/Account/CreateBankAccountAction.php
@@ -4,21 +4,17 @@ class CreateBankAccountAction extends CAction
     public function run()
     {
         $controller=$this->getController();
-        
-        $model=new Account;
-
-        // Uncomment the following line if AJAX validation is needed
-        // $this->performAjaxValidation($model);
+        $Account=new Account;
 
         if(isset($_POST['Account']))
         {
-            $model->attributes=$_POST['Account'];
-            if($model->save())
-                $this->redirect(array('view','id'=>$model->id));
+            $Account->attributes=$_POST['Account'];
+            //if($Account->save())
+                //$this->redirect(array('view','id'=>$Account->id));
         }
 
-        $controller->render('create',array(
-            'model'=>$model,
+        $controller->render('createBankAccount',array(
+            'Account'=>$Account,
         ));
     }
 }

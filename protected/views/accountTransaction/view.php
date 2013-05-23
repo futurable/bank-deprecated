de7c6ec18a0a440e558435b35e17c7c46b34a7d7
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-05-23 13:25:30 +0300

    Remove menu and breadcrumbs

--- protected/views/accountTransaction/view.php
+++ protected/views/accountTransaction/view.php
@@ -1,21 +1,3 @@
-<?php
-/* @var $this AccountTransactionController */
-/* @var $model AccountTransaction */
-
-$this->breadcrumbs=array(
-	'Account Transactions'=>array('index'),
-	$model->id,
-);
-
-$this->menu=array(
-	array('label'=>'List AccountTransaction', 'url'=>array('index')),
-	array('label'=>'Create AccountTransaction', 'url'=>array('create')),
-	array('label'=>'Update AccountTransaction', 'url'=>array('update', 'id'=>$model->id)),
-	array('label'=>'Delete AccountTransaction', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
-	array('label'=>'Manage AccountTransaction', 'url'=>array('admin')),
-);
-?>
-
 <h1>View AccountTransaction #<?php echo $model->id; ?></h1>
 
 <?php $this->widget('zii.widgets.CDetailView', array(

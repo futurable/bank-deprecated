3b78db53ccd5f011b4e554a773a618e329e1bd87
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-05-23 15:25:02 +0300

    Fix return value

--- protected/components/BankSaldo.php
+++ protected/components/BankSaldo.php
@@ -2,15 +2,15 @@
 class BankSaldo {
     public function getAccountSaldo($iban){
         // Count bank saldo       
-        $record =
-        Yii::app()->db->createCommand()
+        $record = Yii::app()->db->createCommand()
         ->select("sum(if( recipient_iban = '$iban', amount, -amount )) AS saldo")
         ->from('bank_account_transaction')
         ->where("payer_iban = '$iban'")
         ->orWhere("recipient_iban  = '$iban'")
         ->queryRow();
         
-        return $record->saldo;
+        if(!empty($record)) return $record['saldo'];
+        else return 0;
     } 
 }
 ?>

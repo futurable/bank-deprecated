b8bd1da120cd3296bb952a28851dc54d2b344edc
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-05-23 16:15:51 +0300

    Add ibandropdown

--- protected/views/accountTransaction/create.php
+++ protected/views/accountTransaction/create.php
@@ -2,6 +2,6 @@
 
 <?php
 if($accountTransaction->form_step === 1) echo $this->renderPartial('_formStep1', array('accountTransaction'=>$accountTransaction));
-elseif($accountTransaction->form_step === 2) echo $this->renderPartial('_formStep2', array('accountTransaction'=>$accountTransaction));
+elseif($accountTransaction->form_step === 2) echo $this->renderPartial('_formStep2', array('accountTransaction'=>$accountTransaction, 'ibanDropdown'=>$ibanDropdown));
 else echo "Error while deciding form step";
 ?>

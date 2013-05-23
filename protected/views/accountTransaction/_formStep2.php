8677fa63d482e7b85e906004879dbf5da9f8a7d4
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-05-23 09:23:31 +0300

    Add payername

--- protected/views/accountTransaction/_formStep2.php
+++ protected/views/accountTransaction/_formStep2.php
@@ -19,73 +19,79 @@ $form=$this->beginWidget('bootstrap.widgets.TbActiveForm', array(
     <fieldset>
     <legend><?php echo Yii::t('AccountTransaction', 'PayerInfo')?></legend>
     	<div class="row">
-		<?php echo $form->label($accountTransaction,'bank_account_id'); ?>
-                <?php $condition = 'bank_user_id=:bank_user_id'; ?>
-                <?php $params = array(':bank_user_id'=>$this->WebUser->id); ?>
-		<?php echo $form->dropDownList($accountTransaction, 'bank_account_id', CHtml::listData(Account::model()->findAll(array('condition'=>$condition, 'params'=>$params)),'id','iban'));?>
-		<?php echo $form->error($accountTransaction,'bank_account_id'); ?>
-	</div>    
+            <?php echo $form->label($accountTransaction,'bank_account_id'); ?>
+            <?php $condition = 'bank_user_id=:bank_user_id'; ?>
+            <?php $params = array(':bank_user_id'=>$this->WebUser->id); ?>
+            <?php echo $form->dropDownList($accountTransaction, 'bank_account_id', CHtml::listData(Account::model()->findAll(array('condition'=>$condition, 'params'=>$params)),'id','iban'));?>
+            <?php echo $form->error($accountTransaction,'bank_account_id'); ?>
+	</div>
+    
+        <div class="row">
+            <?php echo CHTML::label(Yii::t('AccountTransaction', 'PayerName'),'payer_name'); ?>
+            <?php $payerName = $this->WebUser->profile->firstname." ".$this->WebUser->profile->lastname; ?>
+            <?php echo CHTML::textField(Yii::t('AccountTransaction', 'PayerName'),$payerName,array('readonly'=>true)); ?>
+        </div>
     
     <legend><?php echo Yii::t('AccountTransaction', 'RecipientInfo')?></legend>
 	<div class="row">
-		<?php echo $form->label($accountTransaction,'recipient_iban'); ?>
-		<?php echo $form->textField($accountTransaction,'recipient_iban',array('readonly'=>true)); ?>
-		<?php echo $form->error($accountTransaction,'recipient_iban'); ?>
+            <?php echo $form->label($accountTransaction,'recipient_iban'); ?>
+            <?php echo $form->textField($accountTransaction,'recipient_iban',array('readonly'=>true)); ?>
+            <?php echo $form->error($accountTransaction,'recipient_iban'); ?>
 	</div>
 
 	<div class="row">
-		<?php echo $form->label($accountTransaction,'recipient_bic'); ?>
-		<?php echo $form->textField($accountTransaction,'recipient_bic',array('readonly'=>true)); ?>
-		<?php echo $form->error($accountTransaction,'recipient_bic'); ?>
+            <?php echo $form->label($accountTransaction,'recipient_bic'); ?>
+            <?php echo $form->textField($accountTransaction,'recipient_bic',array('readonly'=>true)); ?>
+            <?php echo $form->error($accountTransaction,'recipient_bic'); ?>
 	</div>
 
 	<div class="row">
-		<?php echo $form->label($accountTransaction,'recipient_name'); ?>
-		<?php echo $form->textField($accountTransaction,'recipient_name',array('size'=>35,'maxlength'=>35)); ?>
-		<?php echo $form->error($accountTransaction,'recipient_name'); ?>
+            <?php echo $form->label($accountTransaction,'recipient_name'); ?>
+            <?php echo $form->textField($accountTransaction,'recipient_name',array('size'=>35,'maxlength'=>35)); ?>
+            <?php echo $form->error($accountTransaction,'recipient_name'); ?>
 	</div>
 
     <legend><?php echo Yii::t('AccountTransaction', 'PaymentInfo')?></legend>
 	<div class="row">
-		<?php echo $form->label($accountTransaction,'event_date'); ?>
-                <?php
-                    $this->widget('zii.widgets.jui.CJuiDatePicker', array(
-                        'model'=>$accountTransaction,
-                        'attribute'=>'event_date',
-                        // additional javascript options for the date picker plugin
-                        'options'=>array(
-                            'showAnim'=>'fold',
-                            'dateFormat'=>'dd.mm.yy',
-                            'defaultDate'=>$accountTransaction->event_date,
-                        ),
-                        'htmlOptions'=>array(
-                            'style'=>'height:20px;'
-                        ),
-                    ));
-                ?>
-                <?php echo $form->error($accountTransaction,'event_date'); ?>
+            <?php echo $form->label($accountTransaction,'event_date'); ?>
+            <?php
+                $this->widget('zii.widgets.jui.CJuiDatePicker', array(
+                    'model'=>$accountTransaction,
+                    'attribute'=>'event_date',
+                    // additional javascript options for the date picker plugin
+                    'options'=>array(
+                        'showAnim'=>'fold',
+                        'dateFormat'=>'dd.mm.yy',
+                        'defaultDate'=>$accountTransaction->event_date,
+                    ),
+                    'htmlOptions'=>array(
+                        'style'=>'height:20px;'
+                    ),
+                ));
+            ?>
+            <?php echo $form->error($accountTransaction,'event_date'); ?>
 	</div>
 
 	<div class="row">
-		<?php echo $form->label($accountTransaction,'amount'); ?>
-		<?php echo $form->textField($accountTransaction,'amount',array('size'=>19,'maxlength'=>19)); ?>
-		<?php echo $form->error($accountTransaction,'amount'); ?>
+            <?php echo $form->label($accountTransaction,'amount'); ?>
+            <?php echo $form->textField($accountTransaction,'amount',array('size'=>19,'maxlength'=>19)); ?>
+            <?php echo $form->error($accountTransaction,'amount'); ?>
 	</div>
 
 	<div class="row">
-		<?php echo $form->label($accountTransaction,'reference_number'); ?>
-		<?php echo $form->textField($accountTransaction,'reference_number',array('size'=>20,'maxlength'=>20)); ?>
-		<?php echo $form->error($accountTransaction,'reference_number'); ?>
+            <?php echo $form->label($accountTransaction,'reference_number'); ?>
+            <?php echo $form->textField($accountTransaction,'reference_number',array('size'=>20,'maxlength'=>20)); ?>
+            <?php echo $form->error($accountTransaction,'reference_number'); ?>
 	</div>
 
 	<div class="row">
-		<?php echo $form->label($accountTransaction,'message'); ?>
-		<?php echo $form->textField($accountTransaction,'message',array('size'=>60,'maxlength'=>420)); ?>
-		<?php echo $form->error($accountTransaction,'message'); ?>
+            <?php echo $form->label($accountTransaction,'message'); ?>
+            <?php echo $form->textField($accountTransaction,'message',array('size'=>60,'maxlength'=>420)); ?>
+            <?php echo $form->error($accountTransaction,'message'); ?>
 	</div>
 
 	<div class="row buttons">
-		<?php echo CHtml::submitButton($accountTransaction->isNewRecord ? 'Create' : 'Save'); ?>
+            <?php echo CHtml::submitButton($accountTransaction->isNewRecord ? 'Create' : 'Save'); ?>
 	</div>
     </fieldset>
 <?php $this->endWidget(); ?>

6ca56a5aaee087884f4bf49588c901ba7b1ffc85
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-05-24 14:32:48 +0300

    Add transactions

--- protected/views/accountTransaction/list.php
+++ protected/views/accountTransaction/list.php
@@ -1,3 +1,4 @@
 <h1><?php echo Yii::t('AccountTransaction', 'Transactions'); ?></h1>
 
-<?php echo $this->renderPartial('_listAccounts', array('account'=>$account, 'ibanDropdown'=>$ibanDropdown)) ?>
+<?php echo $this->renderPartial('_listAccounts', array('Account'=>$Account, 'ibanDropdown'=>$ibanDropdown)); ?>
+<?php if(is_array($AccountTransactions)) echo $this->renderPartial('_listTransactions', array('AccountTransactions'=>$AccountTransactions)); ?>

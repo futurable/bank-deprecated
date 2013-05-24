e6ee953a00a27e2b92c563d81b40d0162d276039
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-05-24 12:06:34 +0300

    Add autosubmit

--- protected/views/accountTransaction/_listAccounts.php
+++ protected/views/accountTransaction/_listAccounts.php
@@ -2,19 +2,14 @@
 
 <?php $form=$this->beginWidget('CActiveForm', array(
 	'id'=>'account-form',
-	'enableAjaxValidation'=>false,
 )); ?>
 
 	<div class="row">
                 <?php echo $form->label($account,'iban'); ?>
-                <?php echo $form->dropDownList($account, 'iban', $ibanDropdown, array('class'=>'ibanDropdown'));?>
+                <?php echo $form->dropDownList($account, 'iban', $ibanDropdown, array('class'=>'ibanDropdown', 'submit'=>''));?>
                 <?php echo $form->error($account,'iban'); ?>
 	</div>
 
-	<div class="row buttons">
-		<?php echo CHtml::submitButton(Yii::t('Account', 'GetTransactions')); ?>
-	</div>
-
-<?php $this->endWidget(); ?>
+<?php $this->endWidget();?>
 
 </div><!-- form -->

000427e8183039520172242fc5db8df404002358
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-05-24 14:01:22 +0300

    Add datepickers

--- protected/views/accountTransaction/_listAccounts.php
+++ protected/views/accountTransaction/_listAccounts.php
@@ -4,12 +4,39 @@
 	'id'=>'account-form',
 )); ?>
 
-	<div class="row">
-                <?php echo $form->label($account,'iban'); ?>
-                <?php echo $form->dropDownList($account, 'iban', $ibanDropdown, array('class'=>'ibanDropdown', 'submit'=>''));?>
-                <?php echo $form->error($account,'iban'); ?>
-	</div>
+    <div class="row">
+            <?php echo $form->dropDownList($account, 'iban', $ibanDropdown, array('class'=>'ibanDropdown', 'submit'=>''));?>
+    </div>
+    
+    <div class="row">
+        <?php 
+        $this->widget('zii.widgets.jui.CJuiDatePicker', array(
+            'model'=>$account,
+            'attribute'=>'start_date',
+            // additional javascript options for the date picker plugin
+            'options'=>array(
+                'showAnim'=>'fold',
+                'dateFormat'=>'dd.mm.yy',
+                'maxDate'=>date('d.m.Y'),
+                'firstDay'=>1,
+            ),
+        ));
+
+        $this->widget('zii.widgets.jui.CJuiDatePicker', array(
+            'model'=>$account,
+            'attribute'=>'end_date',
+            // additional javascript options for the date picker plugin
+            'options'=>array(
+                'showAnim'=>'fold',
+                'dateFormat'=>'dd.mm.yy',
+                'maxDate'=>date('d.m.y'),
+                'firstDay'=>1,
+            ),
+        ));
+        ?>
+            
+    </div>
 
 <?php $this->endWidget();?>
-
+<?php echo $account->start_date." ".$account->end_date." ".$account->iban; ?>
 </div><!-- form -->

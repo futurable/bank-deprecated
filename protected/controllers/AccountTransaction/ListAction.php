ccf5b260be7cf7454f9cbc8127de30f0cf75490b
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-05-24 14:32:39 +0300

    Add gettransactions

--- protected/controllers/AccountTransaction/ListAction.php
+++ protected/controllers/AccountTransaction/ListAction.php
@@ -3,19 +3,32 @@ class ListAction extends CAction{
     public function run(){
         $controller=$this->getController();
         
-        $account = new Account();
+        $Account = new Account();
+        $AccountTransactions = null;
         $ibanDropdown = $controller->getIbanDropdown();
-        $action = 'list';
+        
         if(isset($_POST['Account'])){
-            $account->attributes=$_POST['Account'];
+            $Account->attributes=$_POST['Account'];
+            $AccountTransactions=new CActiveDataProvider('AccountTransaction');
+            //$AccountTransactions = $this->getTransactions($Account->iban);
         }
         
         $controller->render('list',array(
-            'account'=>$account,
-            'action'=>$action,
+            'Account'=>$Account,
+            'AccountTransactions'=>$AccountTransactions,
             'ibanDropdown'=>$ibanDropdown,
         ));
-    }   
+    }
+    
+    private function getTransactions($iban){
+            $record=AccountTransaction::model()->findAll(array(
+                'select'=>'recipient_iban, recipient_name, amount, reference_number, message, currency',
+                'condition'=>'payer_iban=:iban OR recipient_iban=:iban',
+                'params'=>array(':iban'=>$iban),
+            ));
+            
+            return $record;
+    }
 }
 
 ?>

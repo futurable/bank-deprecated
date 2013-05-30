481be1eb8808b995c3212b54a95e5d544c7e3c99
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-05-30 15:40:03 +0300

    Remove obsolete fields Add amount Add type

--- protected/views/account/_createLoanApplication.php
+++ protected/views/account/_createLoanApplication.php
@@ -7,60 +7,20 @@
         'htmlOptions'=>array('class'=>'well'),
 )); ?>
 
-	<p class="note">Fields with <span class="required">*</span> are required.</p>
-
-	<?php echo $form->errorSummary($loanAccount); ?>
-
 	<div class="row">
-		<?php echo $form->labelEx($loanAccount,'iban'); ?>
-		<?php echo $form->textField($loanAccount,'iban',array('readonly'=>true)); ?>
-		<?php echo $form->error($loanAccount,'iban'); ?>
+		<?php echo $form->label($loanInfo,'amount'); ?>
+		<?php echo $form->textField($loanInfo,'amount'); ?>
+		<?php echo $form->error($loanInfo,'amount'); ?>
 	</div>
 
-	<div class="row">
-		<?php echo $form->labelEx($loanAccount,'name'); ?>
-		<?php echo $form->textField($loanAccount,'name',array('size'=>60,'maxlength'=>64)); ?>
-		<?php echo $form->error($loanAccount,'name'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($loanAccount,'status'); ?>
-		<?php echo $form->textField($loanAccount,'status',array('readonly'=>true)); ?>
-		<?php echo $form->error($loanAccount,'status'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($loanAccount,'bank_user_id'); ?>
-                <?php echo $form->dropDownList($loanAccount, 'bank_user_id', CHtml::listData(User::model()->findAll(),'id','username'),array('prompt'=>'- Select user -'));?>
-		<?php echo $form->error($loanAccount,'bank_user_id'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($loanAccount,'bank_bic_id'); ?>
-		<?php echo $form->textField($loanAccount,'bank_bic_id', array('readonly'=>true)); ?>
-		<?php echo $form->error($loanAccount,'bank_bic_id'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($loanAccount,'bank_currency_id'); ?>
-		<?php echo $form->dropDownList($loanAccount, 'bank_currency_id', CHtml::listData(Currency::model()->findAll(),'id','code'),array('prompt'=>'- Select currency -'));?>
-		<?php echo $form->error($loanAccount,'bank_currency_id'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($loanAccount,'bank_account_type_id'); ?>
-		<?php echo $form->dropDownList($loanAccount, 'bank_account_type_id', CHtml::listData(AccountType::model()->findAll(),'id','type'),array('prompt'=>'- Select account type -'));?>
-		<?php echo $form->error($loanAccount,'bank_account_type_id'); ?>
-	</div>
-        
         <div class="row">
-		<?php echo $form->labelEx($loanAccount,'bank_interest_id'); ?>
-		<?php echo $form->dropDownList($loanAccount, 'bank_interest_id', CHtml::listData(Interest::model()->findAll(),'id','type'),array('prompt'=>'- Select interest -'));?>
-		<?php echo $form->error($loanAccount,'bank_interest_id'); ?>
+		<?php echo $form->label($loanInfo,'type'); ?>
+		<?php echo $form->textField($loanInfo,'type'); ?>
+		<?php echo $form->error($loanInfo,'type'); ?>
 	</div>
 
 	<div class="row buttons">
-		<?php echo CHtml::submitButton($loanAccount->isNewRecord ? 'Create' : 'Save'); ?>
+		<?php echo CHtml::submitButton('Create'); ?>
 	</div>
 
 <?php $this->endWidget(); ?>

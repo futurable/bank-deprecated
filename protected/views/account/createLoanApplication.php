120deeea9e79c3d9f8deaa3c853b9485b55c90bf
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-05-30 15:38:19 +0300

    Add loaninfo

--- protected/views/account/createLoanApplication.php
+++ protected/views/account/createLoanApplication.php
@@ -1,3 +1,3 @@
 <h1>Create a Loan Application</h1>
 
-<?php echo $this->renderPartial('_createLoanApplication', array('loanAccount'=>$loanAccount)); ?>
+<?php echo $this->renderPartial('_createLoanApplication', array('loanInfo'=>$loanInfo, 'loanAccount'=>$loanAccount)); ?>

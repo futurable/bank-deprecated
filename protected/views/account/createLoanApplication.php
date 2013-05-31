11813a0eb15e9c3da389a9c46406ef42ace2aee9
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-05-31 10:55:16 +0300

    Add translation

--- protected/views/account/createLoanApplication.php
+++ protected/views/account/createLoanApplication.php
@@ -1,3 +1,3 @@
-<h1>Create a Loan Application</h1>
+<h1><?php echo Yii::t('Loan', 'CreateLoanApplication'); ?></h1>
 
 <?php echo $this->renderPartial('_createLoanApplication', array('loanInfo'=>$loanInfo, 'loanAccount'=>$loanAccount)); ?>

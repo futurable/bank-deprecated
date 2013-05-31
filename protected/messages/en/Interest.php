7cdff63f7aa042227db6be8a6ee1bed42eee979d
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-05-31 10:37:22 +0300

    Add interest translations

---
+++ protected/messages/en/Interest.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+    'loanFixed' => 'Fixed interest',
+    'loanEuribor1' => '1 month Euribor',
+    'loanEuribor3' => '3 month Euribor',
+    'loanEuribor6' => '6 month Euribor',
+    'loanEuribor12' => '12 month Euribor',
+);
+?>

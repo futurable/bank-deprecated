d0f056626a9e44b63a939a3d726eca66c62e2230
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-05-31 16:37:23 +0300

    Add saving

--- protected/controllers/Account/CreateLoanApplicationAction.php
+++ protected/controllers/Account/CreateLoanApplicationAction.php
@@ -7,6 +7,37 @@ class CreateLoanApplicationAction extends CAction
         
         $loanAccount = new Account();
         $loanInfo = new Loan();
+
+        if(isset($_POST['Loan']))
+        {
+            $loanUser = User::model()->findbyPk( Yii::app()->user->id );
+            
+            // Account attributes
+            $branchCode = '970300'; // TODO: get this from conf
+            $bban = BBANComponent::generateFinnishBBANaccount($branchCode);
+            $bbanAccountNumber = substr($bban, -6);
+            $iban = IBANComponent::generateFinnishIBANaccount($branchCode, $bbanAccountNumber);
+            $loanAccount->iban = $iban;
+            $loanAccount->name= "Loan account";
+            $loanAccount->bank_user_id = $loanUser->id;
+            
+            $transaction = Yii::app()->db->beginTransaction();
+            $accountSuccess = $loanAccount->save();
+            
+            $loanInfo->attributes=$_POST['Loan'];
+            $loanInfo->bank_account_id=$loanAccount->id;
+            
+            $loanSuccess = $loanInfo->save();
+            
+            if($accountSuccess AND $loanSuccess){
+                $transaction->commit();
+                $controller->redirect(array('view','id'=>$loanAccount->id));
+            }
+            else{
+                $transaction->rollback();
+            };       
+        }
+        
         $controller->render('createLoanApplication',array(
             'loanInfo'=>$loanInfo,
             'loanAccount'=>$loanAccount,

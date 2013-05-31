6da11e1fc6823e0f426e14a9c642cb72d06d3687
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-05-31 19:44:33 +0300

    Add translations

---
+++ protected/messages/en/Menu.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+    'FrontPage' => 'Front page',
+    'NewTransaction' => 'New transaction',
+    'PaymentsForDue' => 'Payments for due',
+    'Transactions' => 'Transactions',
+    'LoanApplications' => 'Loan applications',
+    'Loans' => 'Loans',
+);
+?>
---
+++ protected/messages/fi/Menu.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+    'FrontPage' => 'Etusivu',
+    'NewTransaction' => 'Uusi maksu',
+    'PaymentsForDue' => 'Erääntyvät maksut',
+    'Transactions' => 'Tapahtumat',
+    'LoanApplications' => 'Lainahakemukset',
+    'Loans' => 'Lainat',
+);
+?>

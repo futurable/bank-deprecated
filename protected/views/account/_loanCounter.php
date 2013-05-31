45297d97e81f687410ea991c097916a13d31bb62
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-05-31 15:00:26 +0300

    Remove obsolete field

--- protected/views/account/_loanCounter.php
+++ protected/views/account/_loanCounter.php
@@ -4,14 +4,12 @@
             <th><?php echo Yii::t('Loan', 'Amount') ?></th>
             <th><?php echo Yii::t('Loan', 'TotalInterest') ?></th>
             <th><?php echo Yii::t('Loan', 'RealAmount') ?></th>
-            <th><?php echo Yii::t('Loan', 'Instalment') ?></th>
             <th><?php echo Yii::t('Loan', 'Term') ?></th>
         </tr>
         <tr>
             <td id='loanAmountTd'>0</td>
             <td id='interestAmountTd'>0</td>
             <td id='realAmountTd'>0</td>
-            <td id='repaymentTd'>0</td>
             <td id='termTd'>0</td>
         </tr>
     </table>	

9d5654c449bea38029996f9cf978f7a0c5209493
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-05-31 13:30:28 +0300

    Add bank loan counter

--- js/BankLoanCounter.js
+++ js/BankLoanCounter.js
@@ -1,3 +1,27 @@
 $(document).ready(function(){ 
     
+    loanCounter = function(){
+        // Get variables
+        var loanAmount = $("#Loan_amount").val();
+		var loanType = $("#Loan_type option:selected").val();
+        var loanRepayment = $("#Loan_repayment");
+        var loanInstalment = $("#Loan_instalment");
+		var loanInterval = $("#Loan_interval").val();
+        var loanTerm = $("#Loan_term option:selected").text();
+        var loanTermInterval = $("#Loan_term_interval option:selected").val();
+        var interestType = $("#Loan_bank_interest_id option:selected").val();
+        var loanInterest = $("#Loan_bank_interest_id option:selected").text().match(/[0-9][.][0-9]+/); 
+        var realAmount = 0;
+        
+    }
+    
+    loanCounter();
+	
+	$("#createLoanApplicationForm input").keyup(function(){
+        loanCounter();	
+	});
+	$("#createLoanApplicationForm select").change(function(){
+        loanCounter();
+	});
+    
 });

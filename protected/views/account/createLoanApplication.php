9608472615c7c9dd0426a72de603f9eca42303aa
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-05-31 11:53:27 +0300

    Add javascript

--- protected/views/account/createLoanApplication.php
+++ protected/views/account/createLoanApplication.php
@@ -1,3 +1,5 @@
+<?php Yii::app()->clientScript->registerScriptFile(Yii::app()->baseUrl . '/js/bankLoanApplication.js'); ?>
+
 <h1><?php echo Yii::t('Loan', 'CreateLoanApplication'); ?></h1>
 
 <?php echo $this->renderPartial('_createLoanApplication', array('loanInfo'=>$loanInfo, 'loanAccount'=>$loanAccount)); ?>

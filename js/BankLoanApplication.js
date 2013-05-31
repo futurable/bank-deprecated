8e9e529f0c6a0c4eabbaffd605d96711016e8235
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-05-31 11:52:58 +0300

    Add field toggling

---
+++ js/BankLoanApplication.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    //$(".instalment").hide();
+    //$(".loanTermSelect").hide();
+
+    var toggleFields = function(){
+        // Show repayment
+        if($("#Loan_type").val() == "fixedRepayment"){
+          $(".loanRepayment").fadeIn("slow");
+            if($(".loanInstalment").is(":visible")){ $(".loanInstalment").hide(); }
+            if($(".loanTerm").is(":visible")){ $(".loanTerm").hide(); }
+        }
+        // Show instalment
+        if($("#Loan_type").val() == "fixedInstalment"){
+            if($(".loanRepayment").is(":visible")){ $(".loanRepayment").hide(); }
+            $(".loanInstalment").fadeIn("slow");
+            if($(".loanTerm").is(":visible")){ $(".loanTerm").hide(); }
+        }
+        // Show term
+        if($("#Loan_type").val() == "annuity"){
+            if($(".loanRepayment").is(":visible")){ $(".loanRepayment").hide(); }
+            if($(".loanInstalment").is(":visible")){ $(".loanInstalment").hide(); }
+            $(".loanTerm").fadeIn("slow");
+        }
+    };
+    
+    toggleFields();
+
+    $("#Loan_type").change(function(){
+        toggleFields();
+    });
+
+});

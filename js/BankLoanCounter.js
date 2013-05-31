8034b86f195a7ca8211bbf0f5d6fee45c00fe547
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-05-31 14:59:57 +0300

    Add loan term

--- js/BankLoanCounter.js
+++ js/BankLoanCounter.js
@@ -19,6 +19,70 @@ $(document).ready(function(){
         return interestPart;
     }
     
+    getIntervalInDays = function( term ){
+        var intervalInDays;
+
+        if(term == "day"){ intervalInDays = 1; }
+        else if(term == "week"){ intervalInDays = 7; }
+        else if(term == "month"){ intervalInDays = 30; }
+        else if(term == "year"){ intervalInDays = 360; }
+        else{ intervalInDays = 0; }
+
+        return intervalInDays; 
+    };
+    
+    formatLoanTermToHumanReadable = function( loanTerm, loanInterval ){
+		var loanIntervalInDays = getIntervalInDays(loanInterval); 
+		term = Math.ceil( loanTerm * loanIntervalInDays);
+
+		var years = 0;
+		var months = 0;
+		var days = 0;
+
+		// Years
+		if( loanInterval == "year" ){
+			years = Math.ceil(term / 360);
+		}
+		else{
+			years = Math.floor(term / 360);
+		}
+		term -= years * 360;
+		
+		// Months
+		if( loanInterval == "day" ){
+			months = Math.floor(term / 30);
+		}
+		else{
+			months = Math.ceil(term / 30);
+			if(months >= 12){ // Months are 12 after rounding
+				years++;
+				months -= 12;
+				term -= 360;
+			}
+		}
+
+		term -= months * 30;
+		// Days
+		if(term > 0){
+			days = Math.ceil(term);
+		}
+		
+		// Formatting
+        // @TODO: get translations
+		prettyTerm = "";
+		if( years > 0){
+			prettyTerm += years + " years";
+		}
+		if( months > 0){
+			prettyTerm += months + " months";
+		}
+		if( days > 0){
+			prettyTerm += days + " days";
+		}
+		
+		return prettyTerm;
+	}
+    
     loanCounter = function(){
         // Get variables
         var loanAmount = $("#Loan_amount").val();
@@ -32,10 +96,11 @@ $(document).ready(function(){
         var loanInterest = $("#Loan_bank_interest_id option:selected").text().match(/[0-9][.][0-9]+/) / 100; 
         
         var loanIntervalInDays = getIntervalInDays(loanInterval);
-        var loanInterestPart = getLoanInterestPart(loanInterest, loanIntervalInDays); alert (loanInterestPart);
+        var loanInterestPart = getLoanInterestPart(loanInterest, loanIntervalInDays);
+        var prettyLoanTerm = formatLoanTermToHumanReadable(loanTerm, loanInterval);
         
-        //var realAmount = fillPaymentPlan(loanAmount, loanInterestPart);
-        //fillLoanCounter(loanAmount, realAmount);
+        var realAmount = fillPaymentPlan(loanAmount, loanInterestPart, loanIntervalInDays);
+        fillLoanCounter(loanAmount, realAmount, prettyLoanTerm);
     }
     
     loanCounter();

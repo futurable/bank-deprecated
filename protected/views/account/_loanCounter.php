5270954b9a7872cbc8c04110474ae3cb3b9bc67e
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-05-31 11:29:44 +0300

    Add loan counter frame

---
+++ protected/views/account/_loanCounter.php
@@ -0,0 +1,18 @@
+<div class="well" id='loanCounter'>
+    <table id='loanCounterTable'>
+                    <tr>
+                            <th><?php echo Yii::t('Loan', 'Amount') ?></th>
+                            <th><?php echo Yii::t('Loan', 'Interest') ?></th>
+                            <th><?php echo Yii::t('Loan', 'RealAmount') ?></th>
+                            <th><?php echo Yii::t('Loan', 'Instalment') ?></th>
+                            <th><?php echo Yii::t('Loan', 'Term') ?></th>
+                    </tr>
+                    <tr>
+                            <td id='loanAmountTd'>0</td>
+                            <td id='interestAmountTd'>0</td>
+                            <td id='realAmountTd'>0</td>
+                            <td id='repaymentTd'>0</td>
+                            <td id='termTd'>0</td>
+                    </tr>
+    </table>	
+</div><!-- loanCounterFrame -->

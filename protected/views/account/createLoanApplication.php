b41d68613bb4eb3ceb66ac2f5aa013b3fa5bac68
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-05-31 12:04:31 +0300

    Add payment plan

--- protected/views/account/createLoanApplication.php
+++ protected/views/account/createLoanApplication.php
@@ -3,4 +3,5 @@
 <h1><?php echo Yii::t('Loan', 'CreateLoanApplication'); ?></h1>
 
 <?php echo $this->renderPartial('_createLoanApplication', array('loanInfo'=>$loanInfo, 'loanAccount'=>$loanAccount)); ?>
-<?php echo $this->renderPartial('_loanCounter', array('loanInfo'=>$loanInfo, 'loanAccount'=>$loanAccount)); ?>
+<?php echo $this->renderPartial('_loanCounter'); ?>
+<?php echo $this->renderPartial('_paymentPlan'); ?>

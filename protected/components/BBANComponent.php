c1dd192f01e01d0a598d5c3b2e0475de901f49d2
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-06-02 18:20:56 +0300

    Amend

--- protected/components/BBANComponent.php
+++ protected/components/BBANComponent.php
@@ -47,7 +47,7 @@ Class BBANComponent{
 		$bankAccount = false;
         
         if($accountNumber == false){
-           $accountNumber = BankAccount::model()->count(array('select'=>'id'));
+           $accountNumber = Account::model()->count(array('select'=>'id'));
        }
 
 		// If branchcode and account number are valid 

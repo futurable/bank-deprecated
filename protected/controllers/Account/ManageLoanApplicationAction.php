0793ea260c94f0edf6080955995a85a62e9c9fcd
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-06-03 21:27:30 +0300

    Add loan granting

--- protected/controllers/Account/ManageLoanApplicationAction.php
+++ protected/controllers/Account/ManageLoanApplicationAction.php
@@ -4,10 +4,8 @@ class ManageLoanApplicationAction extends CAction
     public function run()
     {
         $controller=$this->getController();
-        
-        $loanApplications = Loan::model()->findall();
-        
-        if(isset($_POST)&& $_POST['Loan']){
+
+        if(isset($_POST['Loan'])){
             $id = $_POST['Loan']['id'];
             
             $Loan=Loan::model()->findByPk($id);
@@ -17,14 +15,38 @@ class ManageLoanApplicationAction extends CAction
             // Set loan as granted
             $Loan->status = 'active'; // TODO: change this to granted
             $LoanSuccess = $Loan->save();
-
-            // Put money to the account
-            $Account = Account::model()->find(array(
-                'select'=>'id,iban',
-                'condition'=>'bank_user_id=:bankUser',
+            
+            // Get account
+            $AccountId = Account::model()->find(array(
+                'select'=>'id',
+                'condition'=>'bank_user_id=:bankUser AND bank_account_type_id=1',
                 'params'=>array(':bankUser'=>$Loan->bankAccount->bank_user_id),
-            ));
+            ))->id;
+            $Account=Account::model()->findByPk($AccountId);
+            
+            // Make transaction to account
+            $accountTransaction = new AccountTransaction;
+            $accountTransaction->recipient_iban = $Account->iban;
+            $accountTransaction->recipient_bic = "FUTUFIT0"; // @TODO: get this from IBAN
+            $accountTransaction->recipient_name = $Account->bankUser->profile->company;
+            $accountTransaction->payer_iban = "FI8297030000002171"; // @TODO: get this from somewhere
+            $accountTransaction->payer_bic = "FUTUFIT0"; // @TODO: get this from somewhere
+            $accountTransaction->payer_name = "Futural Bank"; // @TODO: get this from somewhere
+            $accountTransaction->event_date = date('d.m.Y');
+            $accountTransaction->amount = $Loan->amount;
+            $accountTransaction->message = "Loan ".$Loan->bankAccount->iban; 
+            
+            $TransactionSuccess = $accountTransaction->save();
+ 
+            if($LoanSuccess && $TransactionSuccess){
+                $transaction->commit();
+            }
+            else{
+                $transaction->rollback();
+            }
         }
+        
+        $loanApplications = Loan::model()->findall();
         
         $controller->render('manageLoanApplication',array(
             'loanApplications' => $loanApplications,

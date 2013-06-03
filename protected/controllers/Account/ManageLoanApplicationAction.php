6a5aacbe676bb8bce280a3d307316d5811c7ac48
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-06-03 20:03:08 +0300

    Add placeholder controller

---
+++ protected/controllers/Account/ManageLoanApplicationAction.php
@@ -0,0 +1,14 @@
+<?php
+class ManageLoanApplicationAction extends CAction
+{
+    public function run()
+    {
+        $controller=$this->getController();
+        
+        $controller->render('manageLoanApplication',array(
+
+        ));
+    }
+}
+
+?>

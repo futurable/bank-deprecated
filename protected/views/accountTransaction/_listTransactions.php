f8918bb464a87477ac0c98e5b7ce877d38d7dc4a
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-06-05 15:17:55 +0300

    Add payer information

--- protected/views/accountTransaction/_listTransactions.php
+++ protected/views/accountTransaction/_listTransactions.php
@@ -1,20 +1,29 @@
 <table>
     <tr>
         <th><?php echo Yii::t('AccountTransaction', 'EventDate') ?></th>
-        <th><?php echo Yii::t('AccountTransaction', 'RecipientName') ?></th>
-        <th><?php echo Yii::t('AccountTransaction', 'RecipientIban') ?></th>
+        <th><?php echo Yii::t('AccountTransaction', 'Payer'); ?></th>
+        <th><?php echo Yii::t('AccountTransaction', 'Recipient'); ?></th>
         <th><?php echo Yii::t('AccountTransaction', 'Amount') ?></th>
-        <th><?php echo Yii::t('AccountTransaction', 'ReferenceNumber')." / ".Yii::t('AccountTransaction', 'Message') ?></th>
+        <th><?php echo Yii::t('AccountTransaction', 'ReferenceNumber')." /<br/>".Yii::t('AccountTransaction', 'Message') ?></th>
     </tr>
     <?php
     foreach($AccountTransactions as $AccountTransaction){
         $eventDateISO = Format::formatISODateToEUROFormat($AccountTransaction->event_date);
         
+        if($AccountTransaction->payer_iban == $Account->iban){
+            $payerIban = $AccountTransaction->payer_iban;
+            $sign = "-";
+        }
+        else{
+            $payerIban = null; // Don't show payer iban due the restrictions of the "law"
+            $sign = "+";
+        }
+        
         echo "<tr>";
             echo "<td>$eventDateISO</td>";
-            echo "<td>$AccountTransaction->recipient_name</td>";
-            echo "<td>$AccountTransaction->recipient_iban</td>";
-            echo "<td>$AccountTransaction->amount $AccountTransaction->currency</td>";
+            echo "<td>$AccountTransaction->payer_name<br/>$payerIban</td>";
+            echo "<td>$AccountTransaction->recipient_name<br/>$AccountTransaction->recipient_iban</td>";
+            echo "<td>{$sign}$AccountTransaction->amount $AccountTransaction->currency</td>";
             echo "<td>$AccountTransaction->reference_number $AccountTransaction->message</td>";
         echo "</tr>";
     }

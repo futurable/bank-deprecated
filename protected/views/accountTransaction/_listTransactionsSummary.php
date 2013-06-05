52726f667f0889b0c35bf6d3402a4708460902db
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-06-05 14:09:33 +0300

    Add end saldo

--- protected/views/accountTransaction/_listTransactionsSummary.php
+++ protected/views/accountTransaction/_listTransactionsSummary.php
@@ -1,6 +1,10 @@
 <table>
     <tr>
-        <td><?php echo Yii::t('AccountTransaction', 'AccountSaldo')." ".date('d.m.Y', strtotime($Account->start_date)); ?></td>
+        <td><?php echo Yii::t('AccountTransaction', 'StartSaldo')." ".date('d.m.Y', strtotime($Account->start_date)); ?></td>
         <td><?php echo BankSaldo::getAccountSaldo($Account->iban, false, $Account->start_date)." ".$Account->bankCurrency->code; ?></td>
+    </tr>
+    <tr>
+        <td><?php echo Yii::t('AccountTransaction', 'EndSaldo')." ".date('d.m.Y', strtotime($Account->end_date)); ?></td>
+        <td><?php echo BankSaldo::getAccountSaldo($Account->iban, false, $Account->end_date)." ".$Account->bankCurrency->code; ?></td>
     </tr>
 </table>

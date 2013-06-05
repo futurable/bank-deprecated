07748096e5a0503be8eb88b38a9465450b93f036
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-06-05 14:08:29 +0300

    Remove transactions summary

--- protected/views/accountTransaction/_listTransactions.php
+++ protected/views/accountTransaction/_listTransactions.php
@@ -1,12 +1,5 @@
 <table>
     <tr>
-        <td><?php echo Yii::t('AccountTransaction', 'AccountSaldo')." ".date('d.m.Y', strtotime($Account->start_date)); ?></td>
-        <td><?php echo BankSaldo::getAccountSaldo($Account->iban, false, $Account->start_date)." ".$Account->bankCurrency->code; ?></td>
-    </tr>
-</table>
-    
-<table>
-    <tr>
         <th><?php echo Yii::t('AccountTransaction', 'EventDate') ?></th>
         <th><?php echo Yii::t('AccountTransaction', 'Amount') ?></th>
     </tr>

dce05e430f006a80ca8184a823c897e357ae77f1
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-06-05 13:58:31 +0300

    Add start_date and end_date to saldo calculation

--- protected/components/BankSaldo.php
+++ protected/components/BankSaldo.php
@@ -1,19 +1,25 @@
 <?php
 class BankSaldo {
-    public function getAccountSaldo($iban){
-        // Count bank saldo       
+    public function getAccountSaldo($iban, $start_date = false, $end_date = false){
+        if(isset($start_date) && DataValidator::isDateISOSyntaxValid($start_date)) return false;
+        if(isset($end_date) && DataValidator::isDateISOSyntaxValid($end_date)) return false;
+        
+        if(!DataValidator::isDateISOSyntaxValid($start_date)) $start_date = date('Y-m-d', 0);
+        if(!DataValidator::isDateISOSyntaxValid($end_date)) $end_date = date('Y-m-d');
+        
+        // Count bank saldo
         $record = Yii::app()->db->createCommand()
         ->select("sum(if( recipient_iban = '$iban', amount, -amount )) AS saldo")
         ->from('bank_account_transaction')
-        ->where("event_date <= now()")
+        ->where("event_date >= '$start_date'")
+        ->andWhere("event_date <= '$end_date'")
         ->andWhere("status = 'active'")
         ->andWhere("(payer_iban = '$iban' OR recipient_iban  = '$iban')")
         ->queryRow();
         
         if(empty($record['saldo'])) $record['saldo'] = "0.00";
         
-        if(!empty($record)) return $record['saldo'];
-        else return 0;
+        return $record['saldo'];
     } 
 }
 ?>

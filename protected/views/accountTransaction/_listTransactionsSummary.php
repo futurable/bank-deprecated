e8ccff682de6318fbd758957c3e16d1e2b0bbcd9
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-06-05 14:11:05 +0300

    Add account number

--- protected/views/accountTransaction/_listTransactionsSummary.php
+++ protected/views/accountTransaction/_listTransactionsSummary.php
@@ -1,10 +1,14 @@
 <table>
     <tr>
-        <td><?php echo Yii::t('AccountTransaction', 'StartSaldo')." ".date('d.m.Y', strtotime($Account->start_date)); ?></td>
+        <td><?php echo Yii::t('Account', 'Account')?></td>
+        <td><?php echo $Account->iban ?></td>
+    </tr>
+    <tr>
+        <td><?php echo Yii::t('Account', 'StartSaldo')." ".date('d.m.Y', strtotime($Account->start_date)); ?></td>
         <td><?php echo BankSaldo::getAccountSaldo($Account->iban, false, $Account->start_date)." ".$Account->bankCurrency->code; ?></td>
     </tr>
     <tr>
-        <td><?php echo Yii::t('AccountTransaction', 'EndSaldo')." ".date('d.m.Y', strtotime($Account->end_date)); ?></td>
+        <td><?php echo Yii::t('Account', 'EndSaldo')." ".date('d.m.Y', strtotime($Account->end_date)); ?></td>
         <td><?php echo BankSaldo::getAccountSaldo($Account->iban, false, $Account->end_date)." ".$Account->bankCurrency->code; ?></td>
     </tr>
 </table>

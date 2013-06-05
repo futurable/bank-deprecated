8a6e146729bd182d7b900238e5099f6a410f32b0
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-06-05 14:06:10 +0300

    Add placeholder listing Add start day saldo

--- protected/views/accountTransaction/_listTransactions.php
+++ protected/views/accountTransaction/_listTransactions.php
@@ -1,1 +1,23 @@
-<?php ?>
+<table>
+    <tr>
+        <td><?php echo Yii::t('AccountTransaction', 'AccountSaldo')." ".date('d.m.Y', strtotime($Account->start_date)); ?></td>
+        <td><?php echo BankSaldo::getAccountSaldo($Account->iban, false, $Account->start_date)." ".$Account->bankCurrency->code; ?></td>
+    </tr>
+</table>
+    
+<table>
+    <tr>
+        <th><?php echo Yii::t('AccountTransaction', 'EventDate') ?></th>
+        <th><?php echo Yii::t('AccountTransaction', 'Amount') ?></th>
+    </tr>
+    <?php
+    foreach($AccountTransactions as $AccountTransaction){
+        $eventDateISO = Format::formatISODateToEUROFormat($AccountTransaction->event_date);
+        
+        echo "<tr>";
+            echo "<td>$eventDateISO</td>";
+            echo "<td>$AccountTransaction->amount</td>";
+        echo "</tr>";
+    }
+    ?>
+</table>

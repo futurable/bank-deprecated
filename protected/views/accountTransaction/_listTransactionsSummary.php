3090e5a8a5cc7a8a08032ac9f40613deb74658eb
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-06-05 14:55:17 +0300

    Fix start saldo

--- protected/views/accountTransaction/_listTransactionsSummary.php
+++ protected/views/accountTransaction/_listTransactionsSummary.php
@@ -1,3 +1,4 @@
+<?php $theDaybefore = strtotime('-1 day', strtotime($Account->start_date));?>
 <table class='well'>
     <tr>
         <td><?php echo Yii::t('Account', 'Account')?></td>
@@ -9,10 +10,10 @@
     </tr>
     <tr>
         <td><?php echo Yii::t('Account', 'StartSaldo')." ".date('d.m.Y', strtotime($Account->start_date)); ?></td>
-        <td><?php echo BankSaldo::getAccountSaldo($Account->iban, false, Format::formatEURODateToISOFormat($Account->start_date))." ".$Account->bankCurrency->code; ?></td>
+        <td><?php echo BankSaldo::getAccountSaldo($Account->iban, date('Y-m-d', $theDaybefore))." ".$Account->bankCurrency->code; ?></td>
     </tr>
     <tr>
         <td><?php echo Yii::t('Account', 'EndSaldo')." ".date('d.m.Y', strtotime($Account->end_date)); ?></td>
-        <td><?php echo BankSaldo::getAccountSaldo($Account->iban, false, Format::formatEURODateToISOFormat($Account->end_date))." ".$Account->bankCurrency->code; ?></td>
+        <td><?php echo BankSaldo::getAccountSaldo($Account->iban, Format::formatEURODateToISOFormat($Account->end_date))." ".$Account->bankCurrency->code; ?></td>
     </tr>
 </table>

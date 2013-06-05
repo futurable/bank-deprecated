74aa820d929bb1af43ea851b0df971675aa7a451
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-06-05 14:37:10 +0300

    Add date formatting

--- protected/views/accountTransaction/_listTransactionsSummary.php
+++ protected/views/accountTransaction/_listTransactionsSummary.php
@@ -9,10 +9,10 @@
     </tr>
     <tr>
         <td><?php echo Yii::t('Account', 'StartSaldo')." ".date('d.m.Y', strtotime($Account->start_date)); ?></td>
-        <td><?php echo BankSaldo::getAccountSaldo($Account->iban, false, $Account->start_date)." ".$Account->bankCurrency->code; ?></td>
+        <td><?php echo BankSaldo::getAccountSaldo($Account->iban, false, Format::formatEURODateToISOFormat($Account->start_date))." ".$Account->bankCurrency->code; ?></td>
     </tr>
     <tr>
         <td><?php echo Yii::t('Account', 'EndSaldo')." ".date('d.m.Y', strtotime($Account->end_date)); ?></td>
-        <td><?php echo BankSaldo::getAccountSaldo($Account->iban, false, $Account->end_date)." ".$Account->bankCurrency->code; ?></td>
+        <td><?php echo BankSaldo::getAccountSaldo($Account->iban, false, Format::formatEURODateToISOFormat($Account->end_date))." ".$Account->bankCurrency->code; ?></td>
     </tr>
 </table>

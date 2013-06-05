f967f0acf91ed76421dfcf3ad41a4bb290d474ea
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-06-05 14:22:18 +0300

    Add fields

--- protected/views/accountTransaction/_listTransactions.php
+++ protected/views/accountTransaction/_listTransactions.php
@@ -1,7 +1,10 @@
 <table>
     <tr>
         <th><?php echo Yii::t('AccountTransaction', 'EventDate') ?></th>
+        <th><?php echo Yii::t('AccountTransaction', 'RecipientName') ?></th>
+        <th><?php echo Yii::t('AccountTransaction', 'RecipientIban') ?></th>
         <th><?php echo Yii::t('AccountTransaction', 'Amount') ?></th>
+        <th><?php echo Yii::t('AccountTransaction', 'ReferenceNumber')." / ".Yii::t('AccountTransaction', 'Message') ?></th>
     </tr>
     <?php
     foreach($AccountTransactions as $AccountTransaction){
@@ -9,7 +12,10 @@
         
         echo "<tr>";
             echo "<td>$eventDateISO</td>";
-            echo "<td>$AccountTransaction->amount</td>";
+            echo "<td>$AccountTransaction->recipient_name</td>";
+            echo "<td>$AccountTransaction->recipient_iban</td>";
+            echo "<td>$AccountTransaction->amount $AccountTransaction->currency</td>";
+            echo "<td>$AccountTransaction->reference_number $AccountTransaction->message</td>";
         echo "</tr>";
     }
     ?>

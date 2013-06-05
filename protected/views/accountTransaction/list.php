736214466a419ba401f6ba3d7c2f4d3da1a5a36b
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-06-05 14:08:24 +0300

    Add transactions summary

--- protected/views/accountTransaction/list.php
+++ protected/views/accountTransaction/list.php
@@ -1,4 +1,7 @@
 <h1><?php echo Yii::t('AccountTransaction', 'Transactions'); ?></h1>
 
 <?php echo $this->renderPartial('_listAccounts', array('Account'=>$Account, 'ibanDropdown'=>$ibanDropdown)); ?>
-<?php if(is_array($AccountTransactions)) echo $this->renderPartial('_listTransactions', array('Account'=>$Account, 'AccountTransactions'=>$AccountTransactions)); ?>
+<?php if(is_array($AccountTransactions)) 
+    echo $this->renderPartial('_listTransactionsSummary', array('Account'=>$Account)); 
+    echo $this->renderPartial('_listTransactions', array('AccountTransactions'=>$AccountTransactions)); 
+?>

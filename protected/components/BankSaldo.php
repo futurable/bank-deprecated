069064817a80be461d82f4bbc4dced5474d8877d
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-06-05 14:52:01 +0300

    Remove start date (as it should be always 0)

--- protected/components/BankSaldo.php
+++ protected/components/BankSaldo.php
@@ -1,15 +1,13 @@
 <?php
 class BankSaldo {
-    public function getAccountSaldo($iban, $start_date = false, $end_date = false){     
-        if(!DataValidator::isDateISOSyntaxValid($start_date)) $start_date = date('Y-m-d', 0);
+    public function getAccountSaldo($iban, $end_date = false){     
         if(!DataValidator::isDateISOSyntaxValid($end_date)) $end_date = date('Y-m-d');
         
         // Count bank saldo
         $record = Yii::app()->db->createCommand()
         ->select("sum(if( recipient_iban = '$iban', amount, -amount )) AS saldo")
         ->from('bank_account_transaction')
-        ->where("event_date >= '$start_date'")
-        ->andWhere("event_date <= '$end_date'")
+        ->where("event_date <= '$end_date'")
         ->andWhere("status = 'active'")
         ->andWhere("(payer_iban = '$iban' OR recipient_iban  = '$iban')")
         ->queryRow();

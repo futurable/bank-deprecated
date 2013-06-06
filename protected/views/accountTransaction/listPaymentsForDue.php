c43e2b3621a129638c81c978698d350785cddc37
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-06-06 11:14:56 +0300

    Modify rendered view

--- protected/views/accountTransaction/listPaymentsForDue.php
+++ protected/views/accountTransaction/listPaymentsForDue.php
@@ -9,23 +9,22 @@
         'id'=>'paymentsForDue',
     )); ?>
 
-        <div class="row">
-            <?php echo $form->dropDownList($Account, 'iban', $ibanDropdown, array('class'=>'ibanDropdown', 'submit'=>''));?>
-        </div>
+    <div class="row">
+        <?php echo $form->dropDownList($Account, 'iban', $ibanDropdown, array('class'=>'ibanDropdown', 'submit'=>''));?>
+    </div>
 
-        <div class="row buttons">
-            <?php $this->widget('bootstrap.widgets.TbButton', array('buttonType'=>'submit', 'label'=>Yii::t('AccountTransaction', 'GetPayments'))); ?>
-        </div>
-
+    <div class="row buttons">
+        <?php $this->widget('bootstrap.widgets.TbButton', array('buttonType'=>'submit', 'label'=>Yii::t('AccountTransaction', 'GetPayments'))); ?>
+    </div>
+        
     <?php $this->endWidget();?>
     </div><!-- form -->
     
     <?php 
     if(is_array($AccountTransactions)){
         if(empty($AccountTransactions)) echo "<p class='light well'>".Yii::t('AccountTransactions', 'NoPaymentsForDue')."</p>";
-        else $this->renderPartial('_listTransactions', array('AccountTransactions'=>$AccountTransactions, 'Account'=>$Account));
+        else $this->renderPartial('_listPaymentsForDue', array('AccountTransactions'=>$AccountTransactions, 'Account'=>$Account));
     }
     ?>
     
 </div><!-- well -->
-

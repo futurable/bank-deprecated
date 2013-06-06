b37579f61d0b9fcc79568a28cbc4554a588f0942
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-06-06 11:06:36 +0300

    Revert modifications

--- protected/views/accountTransaction/_listTransactions.php
+++ protected/views/accountTransaction/_listTransactions.php
@@ -5,13 +5,10 @@
         <th><?php echo Yii::t('AccountTransaction', 'Recipient'); ?></th>
         <th><?php echo Yii::t('AccountTransaction', 'Amount') ?></th>
         <th><?php echo Yii::t('AccountTransaction', 'ReferenceNumber')." /<br/>".Yii::t('AccountTransaction', 'Message') ?></th>
-        <th></th>
     </tr>
     <?php
     foreach($AccountTransactions as $AccountTransaction){
-        $form=$this->beginWidget('CActiveForm');
         $eventDateISO = Format::formatISODateToEUROFormat($AccountTransaction->event_date);
-        echo $form->hiddenField($AccountTransaction,'id');
         
         if($AccountTransaction->payer_iban == $Account->iban){
             $payerIban = $AccountTransaction->payer_iban;
@@ -28,12 +25,7 @@
             echo "<td>$AccountTransaction->recipient_name<br/>$AccountTransaction->recipient_iban</td>";
             echo "<td>{$sign}$AccountTransaction->amount $AccountTransaction->currency</td>";
             echo "<td>$AccountTransaction->reference_number $AccountTransaction->message</td>";
-            echo "<td>";
-                $this->widget('bootstrap.widgets.TbButton', array('buttonType'=>'submit', 'label'=>Yii::t('AccountTransaction','CancelPayment')));
-            echo "</td>";
         echo "</tr>";
-        
-         $this->endWidget();
     }
     ?>
 </table>

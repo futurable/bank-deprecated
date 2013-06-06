674155456389478da5c42c2a9a47b3619073c842
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-06-06 12:53:44 +0300

    Add transaction deleting

--- protected/controllers/AccountTransaction/ListPaymentsForDueAction.php
+++ protected/controllers/AccountTransaction/ListPaymentsForDueAction.php
@@ -8,15 +8,24 @@ class ListPaymentsForDueAction extends CAction{
         $Account->scenario = 'selectAccount';
         $AccountTransactions = null;
         $ibanDropdown = $controller->getIbanDropdown();
-             
-        if(isset($_POST['Account'])){
-            $Account->attributes=$_POST['Account'];
+        
+        if(isset($_POST['AccountTransaction'])){
+            $AccountTransaction = AccountTransaction::model()->findByPk($_POST['AccountTransaction']['id']);
+            $AccountTransaction->status = 'deleted';
+            $AccountTransaction->event_date = Format::formatISODateToEUROFormat($AccountTransaction->event_date);
             
-            $AccountTransactions=AccountTransaction::model()->findAll(array(
-                'condition'=>"status='active' 
-                    AND event_date >= now()
-                    AND ( payer_iban='$Account->iban' OR recipient_iban='$Account->iban' )",
-                'order'=>'event_date'
+            $AccountTransaction->validate();
+            $AccountTransaction->save();
+        }
+        
+        if(isset($_POST['Account'])){
+        $Account->attributes=$_POST['Account'];
+
+        $AccountTransactions=AccountTransaction::model()->findAll(array(
+            'condition'=>"status='active' 
+                AND event_date >= now()
+                AND payer_iban='$Account->iban'",
+            'order'=>'event_date'
             ));
         }
  

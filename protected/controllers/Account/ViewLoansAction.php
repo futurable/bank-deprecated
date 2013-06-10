d21bc95ff1ba433fba2d9d07ba5bdc4d4a2aa2bb
jarmo <jarmo@futurable-jarmo>
2013-06-10 14:33:35 +0300

    Add accounts to render

--- protected/controllers/Account/ViewLoansAction.php
+++ protected/controllers/Account/ViewLoansAction.php
@@ -4,9 +4,15 @@ class ViewLoansAction extends CAction
     public function run()
     {
         $controller=$this->getController();
-
+        
+        $userId = Yii::app()->user->id;
+        $Accounts=Account::model()->findAll(array(
+            'condition'=>"bank_account_type_id=2 
+             AND bank_user_id = '$userId'",
+        ));
+        
         $controller->render('viewLoans',array(
-            
+            'Accounts' => $Accounts,
         ));
     }
 }

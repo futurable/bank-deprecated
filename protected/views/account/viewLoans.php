2feb988b5e067e40e3a26f1e30f469acf839f3eb
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-06-10 17:27:04 +0300

    Add form structure

--- protected/views/account/viewLoans.php
+++ protected/views/account/viewLoans.php
@@ -1,6 +1,31 @@
 <h1><?php echo Yii::t('Account', 'Loans')?></h1>
+
+<table>
+    <tr>
+        <th><?php echo Yii::t('Account', 'AcceptDate'); ?></th>
+        <th><?php echo Yii::t('Account', 'Amount'); ?></th>
+        <th><?php echo Yii::t('Account', 'LoanAccount'); ?></th>
+        <th><?php echo Yii::t('Account', 'Details'); ?></th>
+        <th><?php echo Yii::t('Account', 'PaymentPlan'); ?></th>
+    </tr>
 <?php
 foreach($Accounts as $Account){
-    echo "<p>$Account->iban</p>";
+    $loanInfo = Loan::model()->findbyattributes(array('bank_account_id'=>$Account->id));
+    $form=$this->beginWidget('CActiveForm');
+    
+    echo "<tr>";
+        echo "<td>$loanInfo->accept_date</td>";
+        echo "<td>$loanInfo->amount</td>";
+        echo "<td>$Account->iban</td>";
+        echo "<td>";
+            $this->widget('bootstrap.widgets.TbButton', array('buttonType'=>'submit', 'label'=>Yii::t('Account', 'View')));
+        echo "</td>";
+        echo "<td>";
+            $this->widget('bootstrap.widgets.TbButton', array('buttonType'=>'submit', 'label'=>Yii::t('Account', 'PaymentPlan')));
+        echo "</td>";
+    echo "</tr>";
+    
+    $this->endWidget();
 }
 ?>
+</table>

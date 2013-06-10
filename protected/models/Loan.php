1bf1f8f6bfce61f9fa1060e806e1a53ac9f3721b
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-06-10 17:29:17 +0300

    Add placeholder rules

--- protected/models/Loan.php
+++ protected/models/Loan.php
@@ -53,7 +53,11 @@ class Loan extends CActiveRecord
 			// The following rule is used by search().
 			// @todo Please remove those attributes that should not be searched.
 			array('type, amount, term, instalment, repayment, interval, event_day, create_date, grant_date, accept_date, modify_date, status, bank_interest_id, bank_account_id', 'safe', 'on'=>'search'),
-		);
+            array('create_date','default', 'value'=>new CDbExpression('NOW()'), 'setOnEmpty'=>false,'on'=>'insert'),
+            array('accept_date','default', 'value'=>new CDbExpression('NOW()'), 'setOnEmpty'=>false,'on'=>'update'),
+            array('grant_date','default', 'value'=>new CDbExpression('NOW()'), 'setOnEmpty'=>false,'on'=>'update'),
+            array('modify_date','default', 'value'=>new CDbExpression('NOW()'), 'setOnEmpty'=>false,'on'=>'update'),
+        );
 	}
 
 	/**

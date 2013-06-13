f2d7ca0c2c033cbb718d64339cf9af03be88e78c
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-06-13 16:43:30 +0300

    Fix validity

--- protected/views/account/_paymentPlan.php
+++ protected/views/account/_paymentPlan.php
@@ -8,11 +8,11 @@
             <th><?php echo Yii::t('Loan', 'Principal') ?></th>
         </tr>
         <tr>
-            <td id='loanAmountTd'>0</td>
-            <td id='interestAmountTd'>0</td>
-            <td id='realAmountTd'>0</td>
-            <td id='repaymentTd'>0</td>
-            <td id='termTd'>0</td>
+            <td class='loanAmountTd'>0</td>
+            <td class='interestAmountTd'>0</td>
+            <td class='realAmountTd'>0</td>
+            <td class='repaymentTd'>0</td>
+            <td class='termTd'>0</td>
         </tr>
     </table>	
 </div><!-- paymentPlan -->

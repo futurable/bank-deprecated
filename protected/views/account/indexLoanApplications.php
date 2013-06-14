b5757500232e3e31d12e1933c9cacbaaa2af01af
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-06-14 16:57:29 +0300

    Add action buttons

--- protected/views/account/indexLoanApplications.php
+++ protected/views/account/indexLoanApplications.php
@@ -9,23 +9,35 @@
         <th><?php echo Yii::t('Loan', 'Interest'); ?></th>
         <th><?php echo Yii::t('Loan', 'Interval'); ?></th>
         <th><?php echo Yii::t('Loan', 'Status'); ?></th>
+        <th></th>
     </tr>
     
     <?php
         foreach($loanApplications as $loanApplication){
             echo "<tr>";
                 echo "<td>".Format::formatISODateToEUROFormat($loanApplication->create_date)."</td>";
-                echo "<td>$loanApplication->amount</td>";
+                echo "<td>$loanApplication->amount ".$loanApplication->bankCurrency->code."</td>";
                 echo "<td>".Yii::t('Loan',$loanApplication->type)."</td>";
-                echo "<td>$loanApplication->repayment</td>";
-                echo "<td>".number_format($loanApplication->interest,3)." %</td>";
+                echo "<td>$loanApplication->repayment ".$loanApplication->bankCurrency->code."</td>";
+                echo "<td>".number_format($loanApplication->interest,3)."%</td>";
                 echo "<td>".Yii::t('Loan',$loanApplication->interval)."</td>";
                 echo "<td>".Yii::t('Loan',$loanApplication->status)."</td>";
+                
+                echo "<td>";
+                if($loanApplication->status=='open'){
+                    $this->widget('bootstrap.widgets.TbButton', array('buttonType'=>'submit', 'type'=>'danger', 'label'=>Yii::t('Account', 'Cancel')));
+                }
+                elseif($loanApplication->status=='granted'){
+                    $this->widget('bootstrap.widgets.TbButton', array('buttonType'=>'submit', 'type'=>'success', 'label'=>Yii::t('Account', 'Accept')));
+                    $this->widget('bootstrap.widgets.TbButton', array('buttonType'=>'submit', 'type'=>'danger' ,'label'=>Yii::t('Account', 'Decline')));
+                }
+                echo "</td>";
+                
             echo "</tr>";
         }
     ?>
 </table>
 
 <div class="row buttons">
-    <?php $this->widget('bootstrap.widgets.TbButton', array('url'=>'createLoanApplication', 'label'=>Yii::t('Account', 'CreateLoanApplication'))); ?>
+    <?php $this->widget('bootstrap.widgets.TbButton', array('url'=>'createLoanApplication', 'type'=>'primary', 'label'=>Yii::t('Account', 'CreateLoanApplication'))); ?>
 </div>

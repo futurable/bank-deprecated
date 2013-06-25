f15a0649c06e9aef59eac8d3b80aba0d90f35e40
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-06-25 16:43:07 +0300

    Add translations

--- protected/messages/en/Loan.php
+++ protected/messages/en/Loan.php
@@ -9,7 +9,7 @@
  * Each array element represents the translation (value) of a message (key).
  * If the value is empty, the message is considered as not translated.
  * Messages that no longer need translation will have their translations
- * enclosed between a pair of '@@' marks.
+ * enclosed between a pair of '' marks.
  *
  * Message string can be used with plural forms format. Check i18n section
  * of the guide for details.
@@ -17,12 +17,7 @@
  * NOTE, this file must be saved in UTF-8 encoding.
  */
 return array (
-  '#' => '#',
-  'BankUser' => 'Bank user',
-  'Currency' => 'Currency',
-  'ID' => 'ID',
-  'InterestUpdated' => 'Interest updated',
-  'TermInterval' => 'Term interval',
+  'IncludesMargin' => 'Includes margin',
   'active' => 'Active',
   'annuity' => 'Annuity',
   'day' => 'Day',
@@ -33,7 +28,7 @@ return array (
   'fixedRepayment' => 'Fixed repayment',
   'granted' => 'Granted',
   'month' => 'Month',
-  'months' => '@@months',
+  'months' => 'months',
   'open' => 'Open',
   'paid' => 'Paid',
   'removed' => 'Removed',
@@ -43,18 +38,23 @@ return array (
   'months' => 'months',
   'year' => 'Year',
   'years' => 'years',
+  '#' => '#',
   'AcceptDate' => 'Accept date',
   'Amount' => 'Loan amount',
   'ApplicationCreated' => 'Application created',
   'BankAccount' => 'Bank account',
   'BankInterest' => 'Loan interest',
+  'BankUser' => 'Bank user',
   'CreateDate' => 'Create date',
   'CreateLoanApplication' => 'Create a loan application',
+  'Currency' => 'Currency',
   'EventDay' => 'Event day',
   'GrantDate' => 'Grant date',
+  'ID' => 'ID',
   'IndexLoanApplications' => 'Loan applications',
   'Instalment' => 'Instalment',
   'Interest' => 'Interest',
+  'InterestUpdated' => 'Interest updated',
   'Interval' => 'Interval',
   'ModifyDate' => 'Modify date',
   'Principal' => 'Loan principal',
@@ -62,6 +62,7 @@ return array (
   'Repayment' => 'Repayment',
   'Status' => 'Status',
   'Term' => 'Term',
+  'TermInterval' => 'Term interval',
   'TotalInterest' => 'Total interest',
   'Type' => 'Loan type',
 );

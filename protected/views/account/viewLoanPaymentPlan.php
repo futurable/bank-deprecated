735dd54d0d59a25ed711e6a996cc225fd8e0ddb4
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-08-09 15:52:47 +0300

    Add a placeholder view

---
+++ protected/views/account/viewLoanPaymentPlan.php
@@ -0,0 +1,17 @@
+<h1><?php echo Yii::t('Account', 'LoanPaymentPlan'); ?></h1>
+
+<?php
+    $this->widget('bootstrap.widgets.TbDetailView', array(
+        'data'=>array(
+            'id'=>'LoanDetails', 
+            'LoanAccount'=>$Loan->bankAccount->iban, 
+            'AcceptDate'=> Format::formatISODateToEUROFormat($Loan->accept_date), 
+            'Amount'=>$Loan->amount." ".$Loan->bankCurrency->code,
+        ),
+        'attributes'=>array(
+            array('name'=>'LoanAccount', 'label'=> Yii::t('Account', 'LoanAccount')),
+            array('name'=>'AcceptDate', 'label'=>Yii::t('Account', 'AcceptDate')),
+            array('name'=>'Amount', 'label'=>Yii::t('Account', 'Amount')),
+        ),
+    ));
+?>

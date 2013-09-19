7e44a1a8a9097f6ca47e3a322f2f1d028543b3f7
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-09-19 13:28:45 +0300

    Add company fetching

--- protected/commands/MakeLoanRepaymentsCommand.php
+++ protected/commands/MakeLoanRepaymentsCommand.php
@@ -7,8 +7,20 @@ class MakeLoanRepaymentsCommand extends CConsoleCommand
 {
     public function run($args)
     {   
+        echo( date('Y-m-d H:i:s')." MakeLoanRepayments run started.\n" );
+        
         # 1. Get all loan accounts
         $loanAccounts = Loan::model()->findAll();
+        
+        foreach($loanAccounts as $loanAccount){
+            
+            echo("Using company {$loanAccount->bankUser->profile->company}\n");
+            echo("Using account {$loanAccount->bankAccount->iban}\n");
+            
+            echo("\n");
+        }
+        
+        echo( date('Y-m-d H:i:s')." MakeLoanRepayments run ended.\n\n" );
     }
 }
 ?>

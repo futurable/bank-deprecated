c6d3b2d067864ede9866fb56decb3121684208f3
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-09-19 12:31:25 +0300

    Add base structure

--- protected/commands/MakeLoanRepaymentsCommand.php
+++ protected/commands/MakeLoanRepaymentsCommand.php
@@ -3,4 +3,11 @@
 # 2. Check if payment is due
 # 3. Calculate the remaining loan
 # 4. Make a payment
+class MakeLoanRepaymentsCommand extends CConsoleCommand
+{
+    public function run($args)
+    {   
+        # 1. Get all loan accounts
+        $loanAccounts = Loan::model()->findAll();
+    }
 ?>

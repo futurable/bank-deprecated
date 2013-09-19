2c23ef2858f346cb5237c792fe22728ae1c9d02d
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-09-19 16:02:32 +0300

    Fix accepting the loans

--- protected/controllers/Account/IndexLoanApplicationsAction.php
+++ protected/controllers/Account/IndexLoanApplicationsAction.php
@@ -9,26 +9,34 @@ class IndexLoanApplicationsAction extends CAction{
                 'params'=>array(':bankUser'=>Yii::app()->user->id),
             )
         );
-        
+
         if(isset($_POST['Loan'])){
             $id = $_POST['Loan']['id'];
             
             $Loan=Loan::model()->findByPk($id);
+            $LoanAccount=$Loan->bankAccount;
             $action = $_POST['Loan']['action'];
             $transaction = Yii::app()->db->beginTransaction();
-            
+
             if($action == 'cancel'){
                 $Loan->status = 'removed';
+                $LoanAccount->status = 'removed';
+                
                 $TransactionSuccess = true;
+                $LoanAccountSuccess = true;
             }
             elseif($action=='decline'){
-                $Loan->status = 'declined'; 
-                $TransactionSuccess = true;
-            }
-            if($action == 'accept'){
-                $Loan->status = 'active';
+                $Loan->status = 'declined';
+                $LoanAccount->status = 'removed';
                 
-                // Get account
+                $TransactionSuccess = true;
+                $LoanAccountSuccess = true;
+            }
+            elseif($action == 'accept'){
+                $Loan->status = 'active';
+                $LoanAccount->status = 'enabled';
+                
+                // Get the users first account
                 $AccountId = Account::model()->find(array(
                     'select'=>'id',
                     'condition'=>'bank_user_id=:bankUser AND bank_account_type_id=1',
@@ -38,22 +46,25 @@ class IndexLoanApplicationsAction extends CAction{
                 
                 // Make transaction to account
                 $accountTransaction = new AccountTransaction;
+                
+                $accountTransaction->scenario = 'loanInit'; // Setup scenario so the account amount validator doesn't act up
                 $accountTransaction->recipient_iban = $Account->iban;
-                $accountTransaction->recipient_bic = "FUTUFIT0"; // @TODO: get this from IBAN
+                $accountTransaction->recipient_bic = $Account->bankBic->bic;
                 $accountTransaction->recipient_name = $Account->bankUser->profile->company;
-                $accountTransaction->payer_iban = "FI8297030000002171"; // @TODO: get this from somewhere
-                $accountTransaction->payer_bic = "FUTUFIT0"; // @TODO: get this from somewhere
+                $accountTransaction->payer_iban = $LoanAccount->iban;
+                $accountTransaction->payer_bic = $LoanAccount->bankBic->bic;
                 $accountTransaction->payer_name = "Futural Bank"; // @TODO: get this from somewhere
                 $accountTransaction->event_date = date('d.m.Y');
                 $accountTransaction->amount = $Loan->amount;
                 $accountTransaction->message = "Loan ".$Loan->bankAccount->iban; 
 
                 $TransactionSuccess = $accountTransaction->save();
+                $LoanAccountSuccess = $LoanAccount->save();
             }
             
             $LoanSuccess = $Loan->save();
             
-            if($LoanSuccess && $TransactionSuccess){
+            if($LoanSuccess && $LoanAccountSuccess && $TransactionSuccess){
                 $transaction->commit();
             }
             else{

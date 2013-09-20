64e2957ae24fa3728008667ef7f5ef70188d6b5f
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-09-20 15:26:53 +0300

    Add the functionality to check if payments are due

--- protected/commands/MakeLoanRepaymentsCommand.php
+++ protected/commands/MakeLoanRepaymentsCommand.php
@@ -10,17 +10,46 @@ class MakeLoanRepaymentsCommand extends CConsoleCommand
         echo( date('Y-m-d H:i:s').": MakeLoanRepayments run started.\n" );
         
         # 1. Get all active loans
-        $loans = Loan::model()->findAll(array('condition'=>'status="active" AND id=52'));
+        $loans = Loan::model()->findAll(array('condition'=>'status="active"'));
         
         foreach($loans as $loan){
             $loanTransactions = LoanTransaction::model()->findAll(
                     array(
-                        'condition'=>"bank_loan_id=52", 
+                        'condition'=>"bank_loan_id=:id", 
                         'params'=>array(':id'=>$loan->id),
+                        'order'=>'event_date',
                     ));
             
             echo("Using company {$loan->bankUser->profile->company}\n");
             echo("Using account {$loan->bankAccount->iban}\n");
+            
+            # 2. Check if a payment is due
+            
+            // There are previous payments
+            if(!empty($loanTransactions)){
+                // Last payment
+                $lastPayment = strtotime($loanTransactions[0]->event_date);
+                // @TODO: these should really be made into constants
+                switch($loan->interval){
+                    case 'day':
+                        $interval = 1;
+                        break;
+                    case 'week':
+                        $interval = 7;
+                        break;
+                    case 'month':
+                        $interval = 30;
+                        break;
+                    case 'year':
+                        $inteval = 360;
+                        break;
+                }
+                $interval *= 60*60*24; // interval in seconds
+                $difference = time() - $lastPayment;
+            }
+            if(empty($loanTransactions) OR $difference > $interval ){
+                # 3. Calculate the remaining loan
+            }
             
             echo("\n");
         }

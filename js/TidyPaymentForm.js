2f2987a05721a35604f6ac94905746a1b2f92132
Jarmo Kortetjärvi <jarmo.kortetjarvi@futurable.fi>
2013-11-08 12:22:26 +0200

    Add space trimming

--- js/TidyPaymentForm.js
+++ js/TidyPaymentForm.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){ 
     $("#AccountTransaction_amount").keyup(function(){
         var amount = $("#AccountTransaction_amount").val();
-        var fixedVal = ReplaceCommaWithDot(amount);
+        var trimmedVal = ReplaceSpaceWithEmpty(amount);
+        var fixedVal = ReplaceCommaWithDot(trimmedVal);
         
         $("#AccountTransaction_amount").val(fixedVal);
     });
